18f50df18ade9a1eb5c3ee7bf7c9881f776f2c3b
Alice <aliceroherty@gmail.com>
2023-04-25 23:12:54 -0300

    Implement hamburger menu for mobile devices

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,90 @@
+import { useState } from 'react';
 import Scroll from 'react-scroll';
 const ScrollLink = Scroll.Link;
 
 const Nav = () => {
+	const [open, setOpen] = useState(false);
+
 	return (
 		<nav className='flex justify-between items-center w-screen fixed top-0 bg-[#121212]/[.8] z-50 py-5 px-5 md:px-24'>
-			<ul className='flex flex-row items-center list-none gap-5 m-0 h-full'>
+			<section className='flex lg:hidden'>
+				<div
+					className='space-y-2 [&>*]:hover:bg-gray-400 cursor-pointer [&>*]:transition-colors [&>*]:ease-in-out [&>*]:duration-300'
+					onClick={() => setOpen((prev) => !prev)}
+				>
+					<span className='block h-0.5 w-8 bg-white'></span>
+					<span className='block h-0.5 w-8 bg-white'></span>
+					<span className='block h-0.5 w-8 bg-white'></span>
+				</div>
+
+				<div className={open ? 'showNav' : 'hideNav'}>
+					<div
+						className='absolute top-0 right-0 px-5 py-5 md:px-24'
+						onClick={() => setOpen(false)}
+					>
+						<svg
+							className='h-8 w-8 text-white hover:text-gray-400 transition-colors ease-in-out duration-300 cursor-pointer'
+							viewBox='0 0 24 24'
+							fill='none'
+							stroke='currentColor'
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'
+						>
+							<line x1='18' y1='6' x2='6' y2='18' />
+							<line x1='6' y1='6' x2='18' y2='18' />
+						</svg>
+					</div>
+					<ul className='flex flex-col items-center justify-between min-h-[250px]'>
+						<li>
+							<ScrollLink
+								to='home'
+								className='cursor-pointer text-white text-3xl hover:text-gray-500 transition-colors ease-in-out duration-300'
+								activeClass='active'
+								smooth
+								spy
+							>
+								Home
+							</ScrollLink>
+						</li>
+						<li>
+							<ScrollLink
+								to='about'
+								className='cursor-pointer text-white text-3xl hover:text-gray-500 transition-colors ease-in-out duration-300'
+								activeClass='active'
+								smooth
+								spy
+							>
+								About
+							</ScrollLink>
+						</li>
+						<li>
+							<ScrollLink
+								to='projects'
+								className='cursor-pointer text-white text-3xl hover:text-gray-500 transition-colors ease-in-out duration-300'
+								activeClass='active'
+								smooth
+								spy
+							>
+								Projects
+							</ScrollLink>
+						</li>
+						<li>
+							<ScrollLink
+								to='contact'
+								className='cursor-pointer text-white text-3xl hover:text-gray-500 transition-colors ease-in-out duration-300'
+								activeClass='active'
+								smooth
+								spy
+							>
+								Contact
+							</ScrollLink>
+						</li>
+					</ul>
+				</div>
+			</section>
+
+			<ul className='hidden lg:flex flex-row items-center list-none gap-5 m-0 h-full'>
 				<li>
 					<ScrollLink
 						to='home'
@@ -53,7 +133,9 @@ const Nav = () => {
 			<div>
 				<ScrollLink
 					to='contact'
-					className='bg-pink-400 text-white font-bold p-2 rounded-md px-5 hover:bg-pink-600 transition-colors duration-300 ease-in-out cursor-pointer'
+					className={`${
+						open ? 'hidden text-opacity-0' : ''
+					}bg-pink-400 text-white font-bold p-2 rounded-md px-5 hover:bg-pink-600 transition-colors duration-300 ease-in-out cursor-pointer`}
 					smooth
 				>
 					Hire Now

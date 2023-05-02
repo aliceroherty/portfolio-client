2cd50c13e65190e629d736fe11048f693fb2d25f
Alice <aliceroherty@gmail.com>
2023-05-02 20:31:07 -0300

    Create theme for material ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,34 @@ import Nav from './components/Nav';
 import Home from './components/Home';
 import About from './components/About';
 import { HashRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const muiTheme = createTheme({
+	palette: {
+		mode: 'dark',
+		primary: {
+			main: '#f9a8d4',
+		},
+		secondary: {
+			main: '#db2777',
+		},
+		error: {
+			main: '#ff0081',
+		},
+	},
+});
 
 const App = () => {
 	return (
-		<HashRouter>
-			<div className='select-none'>
-				<Nav />
-				<Home />
-				<About />
-			</div>
-		</HashRouter>
+		<ThemeProvider theme={muiTheme}>
+			<HashRouter>
+				<div className='select-none'>
+					<Nav />
+					<Home />
+					<About />
+				</div>
+			</HashRouter>
+		</ThemeProvider>
 	);
 };
 

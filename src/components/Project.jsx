4bb88831fbe6dbda57712f2f5a9feb1d75050e1f
Alice <aliceroherty@gmail.com>
2023-05-07 16:50:33 -0300

    Create project component
    Relates to #4

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,38 @@
-const Project = () => {
-	return <div>Project</div>;
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
+import { useEffect } from 'react';
+import VanillaTilt from 'vanilla-tilt';
+
+const Project = ({ title, imageUrl, githubUrl, description }) => {
+	useEffect(() => {
+		VanillaTilt.init(document.querySelectorAll('.tilt'), {
+			glare: true,
+			'max-glare': 0.25,
+		});
+	});
+
+	return (
+		<CardActionArea
+			sx={{ borderRadius: '4px' }}
+			className='tilt'
+			href={githubUrl}
+		>
+			<Card className='w-full h-full'>
+				<CardMedia
+					className='h-64'
+					component='img'
+					image={imageUrl}
+					alt={`${title} Image`}
+				/>
+				<CardContent>
+					<h3 className='text-2xl font-bold mb-2'>{title}</h3>
+					<p className='font-light'>{description}</p>
+				</CardContent>
+			</Card>
+		</CardActionArea>
+	);
 };
 
 export default Project;

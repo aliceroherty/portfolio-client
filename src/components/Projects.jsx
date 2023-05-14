ff93a6969c063d1e641402b65d1d6cb08604ec70
Alice <aliceroherty@gmail.com>
2023-05-14 14:37:15 -0300

    Fetch projects from api
    Relates to #4

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,21 +1,58 @@
 import { useEffect, useState } from 'react';
 import Project from './Project';
 import { motion } from 'framer-motion';
+import { login, logout, refresh } from '../utils/authentication';
+import { CircularProgress } from '@mui/material';
 
 const Projects = () => {
 	const [projects, setProjects] = useState([]);
 	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(true);
+
+	const fetchProjects = async () => {
+		try {
+			let tokens = await login();
+
+			if (tokens) {
+				let res = await fetch(
+					`${import.meta.env.VITE_API_URL}/projects`,
+					{
+						headers: {
+							Authorization: `Bearer ${tokens.accessToken}`,
+						},
+					}
+				);
+
+				if (res.status == 200) {
+					const results = await res.json();
+					setProjects(results);
+				} else if (res.status == 403) {
+					tokens = await refresh(tokens.refreshToken);
+					res = await fetch(
+						`${import.meta.env.VITE_API_URL}/projects`,
+						{
+							headers: {
+								Authorization: `Bearer ${tokens.accessToken}`,
+							},
+						}
+					);
+				}
+
+				await logout(tokens.refreshToken);
+			} else {
+				throw new Error('Tokens undefined.');
+			}
+		} catch (error) {
+			setError(true);
+			console.log(error.message);
+		}
+
+		setLoading(false);
+	};
 
 	useEffect(() => {
-		fetch(`${import.meta.env.VITE_API_URL}/projects`)
-			.then((res) => res.json())
-			.then((data) => {
-				setProjects(data);
-			})
-			.catch((error) => {
-				setError(true);
-			});
-	});
+		fetchProjects();
+	}, []);
 
 	return (
 		<motion.div
@@ -29,7 +66,9 @@ const Projects = () => {
 			<h4 className='lg:text-6xl text-5xl font-bold sm:mt-0 mb-16 text-pink-300 text-center lg:text-left'>
 				Projects
 			</h4>
-			{error ? (
+			{loading ? (
+				<CircularProgress />
+			) : error ? (
 				<p>Something went wrong retrieving the projects...</p>
 			) : (
 				<div className='w-full grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 lg:gap-8'>

638454dfe360ca758b73e8ca82ce31650faaac2b
Alice Roherty <alice.roherty@populusgs.com>
2025-04-04 12:10:44 -0400

    Fix and refactor 3D model of keyboard for landing page Fixes #23

---
+++ src/components/Keyboard/KeyboardAnimation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useFrame } from '@react-three/fiber'
+
+const KeyboardAnimation = ({ mesh }) => {
+    KeyboardAnimation.propTypes = {
+        mesh: PropTypes.object.isRequired,
+    }
+
+    // useFrame is a hook from react-three-fiber that allows you to run code on each frame
+    useFrame(() => {
+        mesh.current.rotation.y += 0.005
+    })
+}
+
+export default KeyboardAnimation
